ec1ae1752d407b1dcdc2cc556353a8a2c1a6d95a
ji-mmyliu <jimmyjcl753@gmail.com>
2022-05-23 11:06:40 -0400

    Update new_user.sh

--- dist/static/auto_deploy_scripts/new_user.sh
+++ dist/static/auto_deploy_scripts/new_user.sh
@@ -3,8 +3,10 @@
 echo "==> Please enter a username: "
 read username
 
-
 echo "==> Please enter a password: "
 read password
 
-python3 -c "from auto_deploy import db, User; import bcrypt; user = User(username = $username, password = bcrypt.hashpw(b'$password', bcrypt.gensalt())); db.session.add(user); db.session.commit();"
+clear
+echo "Setting up new user '$username'..."
+
+python3 -c "from auto_deploy import db, User; import bcrypt; user = User(username = '$username', password = bcrypt.hashpw(b'$password', bcrypt.gensalt())); db.session.add(user); db.session.commit();"
