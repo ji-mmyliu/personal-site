00439d5994489746b5aaaa10a61481a46c4bb34a
ji-mmyliu <jimmyjcl753@gmail.com>
2022-06-07 20:32:28 -0400

    Finalize React

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -13,15 +13,18 @@ export const _App = () => {
                 <header className="site-header">
                     <nav className="navbar navbar-expand-md navbar-dark bg-success fixed-top">
                     <div className="container">
-                        <a className="navbar-brand mr-4" href="/">Jimmy Liu's Portfolio</a>
+                        <a className="navbar-brand mr-4" href="/" onClick={(ev)=>{
+                            ev.preventDefault();
+                            navigate("/");
+                        }}>Jimmy Liu's Portfolio</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarToggle">
                         <div className="navbar-nav mr-auto">
-                            <a className="nav-item nav-link" href="/" onClick={(ev)=>{
+                            <a className="nav-item nav-link" href="/about-me" onClick={(ev)=>{
                                 ev.preventDefault();
-                                navigate("/");
+                                navigate("/about-me");
                             }}>About Me</a>
                             <a className="nav-item nav-link" href="/projects" onClick={(ev)=>{
                                 ev.preventDefault();
