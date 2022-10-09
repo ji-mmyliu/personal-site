0726c3a01b048abe1c28f134df72d4e59e7531ca
Jimmy Liu <jimmyjcl753@gmail.com>
2022-10-09 16:18:16 -0400

    Add picoCTF and MaCS

--- src/components/achievements/index.tsx
+++ src/components/achievements/index.tsx
@@ -14,7 +14,8 @@ export const Achievements = () => {
                         
                         <h5>May</h5>
                         <ul>
-                            <li><a href="https://www.ofsaa.on.ca/badminton">Ontario Federation of School Athletic Associations</a> (OFSAA) badminton mixed doubles - "A Flight" silver medal</li>
+                            <li><a href="https://play.picoctf.org/events/70" target="_blank">picoCTF 2022</a> Capture the Flag competition by <a href="https://www.cmu.edu/" target="_blank">CMU</a> 3<sup>rd</sup> place in <a href="https://discover.rbcroyalbank.com/canhack-2022-teaching-students-cyber-security-skills-through-friendly-competition/" target="_blank">CanHack</a></li>
+                            <li><a href="https://www.ofsaa.on.ca/badminton" target="_blank">Ontario Federation of School Athletic Associations</a> (OFSAA) badminton mixed doubles - "A Flight" silver medal</li>
                         </ul>
                         
                         <h5>April</h5>
@@ -25,7 +26,7 @@ export const Achievements = () => {
 
                         <h5>March</h5>
                         <ul>
-                            <li><a href="http://usaco.org">USA Computing Olympiad</a> 2022 <a href="http://usaco.org/index.php?page=open22results">Open Contest</a> Silver Division (Perfect score of 1000)</li>
+                            <li><a href="http://usaco.org" target="_blank">USA Computing Olympiad</a> 2022 <a href="http://usaco.org/index.php?page=open22results" target="_blank">Open Contest</a> Silver Division (Perfect score of 1000)</li>
                         </ul>
         
                         <h5>February</h5>
@@ -52,24 +53,24 @@ export const Achievements = () => {
                         <legend className="border-bottom mb-4"><h3>2021</h3></legend>
                         <h5>August</h5>
                         <ul>
-                            <li><a href="https://ignition-hacks-2021.devpost.com/">Ignition Hacks</a> 2021 Division Delta Best Education Hack</li>
+                            <li><a href="https://ignition-hacks-2021.devpost.com/" target="_blank">Ignition Hacks</a> 2021 Division Delta Best Education Hack</li>
                         </ul>
         
                         <h5>June</h5>
                         <ul>
-                            <li>DMOJ Monthly Open Programming Contest 2020-21 <a href="https://dmoj.ca/contest/dmopc20c7">June Contest</a> - 18<sup>th</sup> place</li>
+                            <li>DMOJ Monthly Open Programming Contest 2020-21 <a href="https://dmoj.ca/contest/dmopc20c7" target="_blank">June Contest</a> - 18<sup>th</sup> place</li>
                             <li>Achieved "Master" rating title on DMOJ with rating 1944</li>
                         </ul>
         
                         <h5>May</h5>
                         <ul>
-                            <li><a href="https://ecoo.mcpt.ca/">ECOO</a> Programming Contest - 37<sup>th</sup> place with score 552</li>
+                            <li><a href="https://ecoo.mcpt.ca/" target="_blank">ECOO</a> Programming Contest - 37<sup>th</sup> place with score 552</li>
                             <li>AP Computer Science A Exam - Score of 5</li>
                         </ul>
         
                         <h5>April</h5>
                         <ul>
-                            <li>DMOJ Monthly Open Programming Contest 2020-21 <a href="https://dmoj.ca/contest/dmopc20c5">April Contest</a> - 36<sup>th</sup> place</li>
+                            <li>DMOJ Monthly Open Programming Contest 2020-21 <a href="https://dmoj.ca/contest/dmopc20c5" target="_blank">April Contest</a> - 36<sup>th</sup> place</li>
                             <li><a href="https://lyonhacks.devpost.com/">LyonHacks</a> Student Hackathon Honourable Mentions Winner</li>
                         </ul>
         
@@ -77,6 +78,12 @@ export const Achievements = () => {
                         <h5>November</h5>
                         <ul>
                             <li>Canadian Intermediate Mathematics Contest - Group 5 Honour Roll</li>
+                        </ul>
+
+                        <legend className="border-bottom mb-4"><h3>2019</h3></legend>
+                        <h5>May</h5>
+                        <ul>
+                            <li>Acceptance into the <a href="http://wlmac.ca/macsprogram/" target="_blank">MaCS</a> enriched Math and Computer Science program at William Lyon Mackenzie Collegiate Institute.</li>
                         </ul>
                     </div>
                 </div>
