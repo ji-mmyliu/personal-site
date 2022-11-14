3b889e9dc01183c824bd6dd211d4ffca8f0a84fe
ji-mmyliu <jimmyjcl753@gmail.com>
2022-11-14 14:33:10 -0500

    Add up and down buttons

--- src/components/notes/index.tsx
+++ src/components/notes/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 
 export const Notes = (): JSX.Element => {
     const [num, setNum] = React.useState(1);
+    const [isInt, setIsInt] = React.useState(true);
     const [error, setError] = React.useState(false);
 
     React.useEffect((): void => {
         document.title = "Notes | Jimmy's Personal Website";
     }, []);
+
+    React.useEffect((): void => {
+        setIsInt(Number.isInteger(parseInt(num.toString())));
+    }, [num]);
 
     const lessonNumberChange = (e): void => {
         setNum(e.target.value);
@@ -24,13 +29,33 @@ export const Notes = (): JSX.Element => {
         return `/notes/finance/lesson${lessonNum}.html`;
     }
 
+    const incrementLesson = (increment: number): void => {
+        if (Number.isInteger(num)) {
+            setNum(num + increment);
+        }
+    }
+
     return (
         <div className="container pl-5 pr-5">
             <article className="card mb-3 notes-form">
                 <form>
                     <div className="form-group">
                         <label htmlFor="lesson-num">Lesson Number</label>
-                        <input type="text" className={`form-control ${!error && "is-valid"}`} id="lesson-num" onChange={lessonNumberChange} aria-describedby="emailHelp" placeholder="Enter lesson number" />
+                        <div className="lesson-input">
+                            <input type="text" className={`form-control ${!error && "is-valid"}`} id="lesson-num" onChange={lessonNumberChange} aria-describedby="emailHelp" placeholder="Enter lesson number" />
+                            {isInt &&
+                                <>
+                                    <button className="btn btn-primary" onClick={(ev) => {
+                                        ev.preventDefault();
+                                        incrementLesson(1);
+                                    }}>Next</button>
+                                    <button className="btn btn-secondary" onClick={(ev) => {
+                                        ev.preventDefault();
+                                        incrementLesson(-1);
+                                    }}>Previous</button>
+                                </>
+                            }
+                        </div>
                         {error && <small id="lesson-num-help" className="form-text error-text">Sorry, lesson not here yet!</small>}
                         <small id="lesson-num-help" className="form-text text-muted">Notes should update automatically below!</small>
                     </div>
