11c3f645b1f2e3692346fbbcf724e96d4126082b
Jimmy Liu <jimmyjcl753@gmail.com>
2022-11-22 18:24:26 -0500

    Add dynamic url for notes

--- src/components/notes/index.tsx
+++ src/components/notes/index.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '../../util/query';
+import * as config from '../../../config';
 
 export const Notes = (): JSX.Element => {
-    const [num, setNum] = React.useState(1);
+    const query: URLSearchParams = useQuery();
+    const lessonId: string | null = query.get("lessonId");
+
+    const [num, setNum] = React.useState((!!lessonId) ? parseInt(lessonId!) : 1);
     const [error, setError] = React.useState(false);
 
     React.useEffect((): void => {
         document.title = "Notes | Jimmy's Personal Website";
+        (document.getElementById("lesson-num")! as any).value = (!!lessonId) ? lessonId! : 1;
     }, []);
 
     const lessonNumberChange = (e): void => {
         setNum(e.target.value);
         checkExists(e.target.value);
+        window.history.replaceState(null, "", `/notes?lessonId=${encodeURIComponent(e.target.value)}`);
     }
 
     const checkExists = (lessonNum: number): void => {
@@ -20,7 +28,7 @@ export const Notes = (): JSX.Element => {
     }
 
     const lessonUrl = (lessonNum: number): string => {
-        return `/notes/finance/lesson${lessonNum}.html`;
+        return `${config.baseURL}/notes/finance/lesson${lessonNum}.html`;
     }
 
     return (
@@ -29,7 +37,7 @@ export const Notes = (): JSX.Element => {
                 <form>
                     <div className="form-group">
                         <label htmlFor="lesson-num">Lesson Number</label>
-                        <input type="text" className={`form-control ${!error && "is-valid"}`} id="lesson-num" onChange={lessonNumberChange} aria-describedby="emailHelp" placeholder="Enter lesson number" />
+                        <input type="number" min={1} className={`form-control ${!error && "is-valid"}`} id="lesson-num" onChange={lessonNumberChange} aria-describedby="emailHelp" placeholder="Enter lesson number" />
                         {error && <small id="lesson-num-help" className="form-text error-text">Sorry, lesson not here yet!</small>}
                         <small id="lesson-num-help" className="form-text text-muted">Notes should update automatically below!</small>
                     </div>
